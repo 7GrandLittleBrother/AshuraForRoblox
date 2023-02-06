6fd7f559b36873fe234b3bab2dde1cd1e0186061
Vampira <73371267+7GrandLittleBrother@users.noreply.github.com>
2023-02-06 22:24:14 +0800

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,3 +1,7 @@
-shared.GuiLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/AshuraForRoblox/main/GuiLibrary.lua", true))()
+local GuiLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/AshuraForRoblox/main/GuiLibrary.lua", true))()
 
--- // nothing else lol
+shared.GuiLibrary = GuiLibrary
+
+if game.PlaceId == 6872274481 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/AshuraForRoblox/main/CustomModules/6872274481.lua", true))()
+end
