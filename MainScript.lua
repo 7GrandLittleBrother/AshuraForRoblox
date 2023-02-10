32230c67d992f8ce6396b3180b9e1c736349fba3
ProfessionalBacon <73371267+7GrandLittleBrother@users.noreply.github.com>
2023-02-11 01:52:35 +0800

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,7 +1,19 @@
 local GuiLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/AshuraForRoblox/main/GuiLibrary.lua", true))()
-
 shared.GuiLibrary = GuiLibrary
 
+local function createnotification(title, text, delay)
+	game:GetService("StarterGui"):SetCore("SendNotification", {
+		Title = title,
+		Text = text,
+		Duration = delay
+	})
+end
+
+if not shared.AshuraExecuted then
+	createnotification("AshuraClient", "Ashura Already Injected", 5)
+end
+shared.AshuraExecuted = true
+
 if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/AshuraForRoblox/main/CustomModules/bedwars.lua", true))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/AshuraForRoblox/main/CustomModules/bedwars.lua", true))()
 end
