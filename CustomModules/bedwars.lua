3235d6d1f7dc049f746a15652e2af1ed0d6ea1f4
ProfessionalBacon <73371267+7GrandLittleBrother@users.noreply.github.com>
2023-02-11 23:47:11 +0800

    Update bedwars.lua

--- CustomModules/bedwars.lua
+++ CustomModules/bedwars.lua
@@ -436,7 +436,7 @@ runcode(function()
 	local antivoidconnection
 	local antivoiding = false
 	local antitransparent = {["Value"] = 50}
-	local anticolor = {["Hue"] = 1, ["Sat"] = 1, ["Value"] = 0.55}
+	local anticolor = {["Red"] = 140, ["Green"] = 0, ["Blue"] = 0}
 	local AntiVoid = {["Enabled"] = false}
 	Sections["AntiVoid"].NewToggle({
 		["Name"] = "AntiVoid",
@@ -486,11 +486,10 @@ runcode(function()
 	})
 	Sections["AntiVoid"].NewColorPicker({
 		["Name"] = "Color",
-		["Default"] = Color3.fromHSV(anticolor["Hue"], anticolor["Sat"], anticolor["Value"])
-		["Function"] = function(val)
-			anticolor["Hue"], anticolor["Sat"], anticolor["Value"] = val
+		["Default"] = Color3.fromHSV(anticolor["Red"], anticolor["Green"], anticolor["Blue"]),
+		["Function"] = function()
 			if antivoidpart then
-				antivoidpart.Color = Color3.fromHSV(anticolor["Hue"], anticolor["Sat"], anticolor["Value"])
+				antivoidpart.Color = Color3.fromHSV(anticolor["Red"], anticolor["Green"], anticolor["Blue"])
 			end
 		end
 	})
