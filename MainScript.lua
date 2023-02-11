f2fb05309d2be003df25c0ea04630e6f0f5db3ed
ProfessionalBacon <73371267+7GrandLittleBrother@users.noreply.github.com>
2023-02-11 14:25:09 +0800

    already inject stuff

--- MainScript.lua
+++ MainScript.lua
@@ -1,6 +1,3 @@
-local GuiLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/AshuraForRoblox/main/GuiLibrary.lua", true))()
-shared.GuiLibrary = GuiLibrary
-
 local function createnotification(title, text, delay)
 	game:GetService("StarterGui"):SetCore("SendNotification", {
 		Title = title,
@@ -9,12 +6,10 @@ local function createnotification(title, text, delay)
 	})
 end
 
-if shared.AshuraExecuted then
-	createnotification("AshuraClient", "Ashura Already Injected", 5)
-	return
-else
-	shared.AshuraExecuted = true
-end
+createnotification("AshuraClient", "Ashura Already Injected", 5)
+shared.AshuraExecuted = true
+
+shared.GuiLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/AshuraForRoblox/main/GuiLibrary.lua", true))()
 
 if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/AshuraForRoblox/main/CustomModules/bedwars.lua", true))()
