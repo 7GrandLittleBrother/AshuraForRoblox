7428eeface6a3d776ca09fefa7dea2953c0957e9
NTDCore <102583640+NTDCore@users.noreply.github.com>
2023-03-04 14:14:45 +0700

    Fixed MainScript

--- MainScript.lua
+++ MainScript.lua
@@ -13,10 +13,8 @@ else
 	shared.AshuraExecuted = true
 end
 
-local GuiLibrary
+local GuiLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/AshuraForRoblox/main/GuiLibrary.lua", true))()
 shared.GuiLibrary = GuiLibrary
-
-shared.GuiLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/AshuraForRoblox/main/GuiLibrary.lua", true))()
 
 if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/AshuraForRoblox/main/CustomModules/bedwars.lua", true))()
